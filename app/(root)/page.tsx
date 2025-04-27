ab3d44205765471d0807e1e62032887da9ed4304
rohitkuna <rohit.kuna@capgemini.com>
2025-04-27 17:48:48 +0530

    feat/nextjs-components adding some layout and page.tsx

---
+++ app/(root)/page.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link"
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+
+const Page=()=>{
+    return(
+        <>
+            <section className="card-cta">
+                <div className="flex flex-col justify-center gap-6 max-w-lg">
+                    <h2>Get Interview Ready with AI Powered Feedbacks</h2>
+                    <p>Practice on real interview questions and get instant feedback</p>
+                    <Button asChild className="btn-primary max-sm:w-full">
+                        <Link href="/interview">Start an Interview</Link>
+                    </Button>
+                </div>
+                <Image src="/robot.png" alt="robot-dude" width={400} height={400} className="max-sm:hidden" />
+            </section>
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Your Interviews</h2>
+                <div className="interviews-section">
+                    <p>You haven't taken any interviews yet</p>
+                </div>
+            </section>
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Your Interviews</h2>
+                <div className="interviews-section">
+                    <p>There are no interviews available</p>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Page
